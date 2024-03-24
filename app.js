a08c9819c92759e43960dd168d2b557c1251f1eb
Muhammad Tayyab <786.mtayyab@gmail.com>
2024-03-24 17:34:59 +0500

    update1

---
+++ app.js
@@ -0,0 +1,105 @@
+//================== Assigment # 5 ==================//
+
+
+// // Question # 1
+
+
+
+
+
+// // Question # 2
+// var arr = [0,1,2,3]
+// document.write(
+
+// arr[0]+" "+[1]+" "+[2]+" "+[3]+"<br>"+
+// [1]+" "+[0]+" "+[1]+" "+[2]+"<br>"+
+// [2]+" "+[1]+" "+[0]+" "+[1]+"<br>"
+
+// )
+
+
+// // Question # 3
+
+// for (var i=1; i<=10; i++){
+//     document.write(i + "<br>")
+// }
+
+// // Question # 4
+
+// var num1 = prompt("Enter a Number to show its Multiplication Table");
+// var lenght = prompt("Multiplication Length of Talbe")
+
+// for (var i=1; i<=lenght; i++){
+// document.write(num1 + "x" + i + "=" + num1 * i + "<br>")
+
+// }
+
+
+// // Question # 5
+
+
+// var fruits = ['apple','banana','mango','orange','strawberry']
+
+// for(i=0; i<=4; i++){
+
+//     document.write (`<h1>${fruits[i]}</h1>
+       
+//     `)
+// }
+
+// document.write(`
+// <h3> Element at index  0 is  ${fruits[0]} </h3> 
+// <h3> Element at index  1 is  ${fruits[1]} </h3> 
+// <h3> Element at index  2 is  ${fruits[2]} </h3> 
+// <h3> Element at index  3 is  ${fruits[3]} </h3> 
+// <h3> Element at index  4 is  ${fruits[4]} </h3> 
+// `)
+
+
+// // Question # 6 (a)
+
+// for (var i=1; i<=15; i++){
+//     document.write(i + ",")
+// }
+
+// // Question # 6 (b)
+
+// for (var i=10; i>=1; i--){
+//     document.write(i + ",")
+// }
+
+
+// // Question # 6 (c)
+
+// for (var i=0; i<=20; i++){
+//     if(i % 2 === 0){
+
+//     document.write(i + ",")}
+// }
+
+// // Question # 6 (d)
+
+// for (var i=0; i<=20; i++){
+//     if(i % 2 !== 0){
+
+//     document.write(i + ",")}
+// }
+
+
+// // Question # 6 (e)
+
+// for (var i=1; i<=20; i++){
+//     if(i % 2 === 0){
+
+//     document.write(i + "K, ")}
+// }
+
+
+
+
+
+    
+    
+
+
+
